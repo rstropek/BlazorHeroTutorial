f1152fa2aabe05233ec6f9def72b615ba7ced63f
Rainer Stropek <rainer@software-architects.at>
2019-09-30 15:19:31 +0200

    Add MathTrainer lab

---
+++ Lab/MathTrainer/Model/ResultLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathTrainer.Model
+{
+    public class ResultLine
+    {
+        public int Base { get; set; }
+
+        public char Operator { get; set; }
+
+        public int Operand { get; set; }
+
+        public int Result { get; set; }
+    }
+}
